c1332dd8663a8fcbb9e2a487703ce9dc8bf9b701
Light1121 <light1121.lin@gmail.com>
2025-08-15 16:11:52 +1000

    feat: homepage scaffold

---
+++ Frontend/WeatherJYJAM_frontend/src/pages/Home/_components/Header/_components/Logo.tsx
@@ -0,0 +1,30 @@
+import type { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import logoImage from '../_asset/WeatherJYJAM_Logo.jpg'
+
+const LogoImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+  border-radius: 8px;
+  cursor: pointer;
+`
+
+const Logo: FC = () => {
+  const navigate = useNavigate()
+
+  const handleLogoClick = () => {
+    navigate('/')
+  }
+
+  return (
+    <LogoImage
+      src={logoImage}
+      alt="WeatherJYJAM Logo"
+      onClick={handleLogoClick}
+    />
+  )
+}
+
+export default Logo
