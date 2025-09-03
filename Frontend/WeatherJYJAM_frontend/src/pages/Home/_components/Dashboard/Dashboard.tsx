7fa2f48ba5db5286c1a6771d421a7c6e43ceea41
Madelyn Ooi Chia Yee <mooi0010@student.monash.edu>
2025-09-04 05:31:27 +1000

    feat: added container

--- Frontend/WeatherJYJAM_frontend/src/pages/Home/_components/Dashboard/Dashboard.tsx
+++ Frontend/WeatherJYJAM_frontend/src/pages/Home/_components/Dashboard/Dashboard.tsx
@@ -3,13 +3,22 @@ import TabSidebar from './TabSidebar'
 import MapView from './MapView'
 import { styled } from 'styled-components'
 
+const Background = styled.div`
+  display: flex;
+  flex: 1;
+  border-radius: 15px;
+  background-color: #f1fafdff;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  overflow: hidden;
+  margin: 20px;
+`
+
 const MapSection = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 20px;
-  position: relative;
   font-family: 'Instrument Sans', sans-serif;
 `
 
@@ -18,10 +27,12 @@ interface DashboardProps {
 }
 
 const Dashboard: FC<DashboardProps> = ({ currentTabId }) => (
-  <MapSection>
-    <TabSidebar currentTabId={currentTabId} />
-    <MapView currentTabId={currentTabId} />
-  </MapSection>
+  <Background>
+    <MapSection>
+      <TabSidebar currentTabId={currentTabId} />
+      <MapView currentTabId={currentTabId} />
+    </MapSection>
+  </Background>
 )
 
 export default Dashboard
