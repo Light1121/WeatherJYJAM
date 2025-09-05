24d71733b609ad13726297fc1220ac3e2cfc0d72
Light1121 <light1121.lin@gmail.com>
2025-09-05 22:35:15 +1000

    feat: basic empty sqlite db init

--- _Backend/app/__init__.py
+++ _Backend/app/__init__.py
@@ -1,13 +1,14 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from flask import Blueprint, Flask
 from flask_cors import CORS
 from flask_restx import Api
 
+from app._utils.db import close_db
 from .user.controller import api as userapi
 
-load_dotenv("./.env")
+load_dotenv("./.env.local")
 api_bp = Blueprint("api", os.getenv("FLASK_APP_NAME", "WeatherJYJAM_app"), url_prefix="/api")
 api = Api(api_bp)
 
@@ -17,12 +18,15 @@ def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
 
-    api.add_namespace(userapi)
-
     # CORS
     CORS(app)
 
+    # close db
+    app.teardown_appcontext(close_db)
+
     # Register blueprint
+    api.add_namespace(userapi)
     app.register_blueprint(api_bp)
 
     return app
+
--- _Backend/app/helloworld/controller.py
+++
@@ -1,7 +0,0 @@
-from flask_restx import Resource
-from ..  import api
-
-@api.route('/helloworld')
-class HelloWorldApi(Resource):
-    def get(self):
-        return "Hello World"
