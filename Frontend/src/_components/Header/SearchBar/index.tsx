6445c1e56687d91bca337349baeaef0009eb7f0d
Light1121 <light1121.lin@gmail.com>
2025-10-10 21:54:08 +1100

    fix: lint fix

--- Frontend/src/_components/Header/SearchBar/index.tsx
+++ Frontend/src/_components/Header/SearchBar/index.tsx
@@ -1,4 +1,5 @@
-export { default as SearchBar } from './SearchBar'
+/* eslint-disable react-refresh/only-export-components */
+export { default, default as SearchBar } from './SearchBar'
 export { default as SearchDropdown } from './SearchDropdown'
 export { default as AIdropdown } from './AIdropdown'
 export { default as SearchSwitch } from './SearchSwitch'
